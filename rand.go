8980a71aab631d30211b5ff278e75208872c2da2
Steven Berlanga <zabawaba99@gmail.com>
2015-03-27 01:23:18 -0400

    renaming global rand object and saving seed value

--- rand.go
+++ rand.go
@@ -7,22 +7,25 @@ import (
 	"time"
 )
 
-var seeder *rand.Rand
+var (
+	gblRand  *rand.Rand
+	randSeed int64
+)
 
 func init() {
 	seed()
 }
 
 func seed() {
-	seed := time.Now.Unix()
+	randSeed = time.Now().Unix()
 	if env := os.Getenv("GOSEED"); env != "" {
 		t, err := strconv.ParseInt(env, 10, 64)
-		if err != nil {
-			seed = t
+		if err == nil {
+			randSeed = t
 		}
 	}
 
-	seeder = rand.New(rand.NewSource(seed))
+	gblRand = rand.New(rand.NewSource(randSeed))
 }
 
 // ExpFloat64 returns an exponentially distributed float64 in the range
@@ -31,34 +34,34 @@ func seed() {
 // a different rate parameter, callers can adjust the output using:
 //
 //		sample = ExpFloat64() / desiredRateParameter
-func ExpFloat64() float64 { return seeder.ExpFloat64() }
+func ExpFloat64() float64 { return gblRand.ExpFloat64() }
 
 // Float32 returns, as a float32, a pseudo-random number in [0.0,1.0).
-func Float32() float32 { return seeder.Float32() }
+func Float32() float32 { return gblRand.Float32() }
 
 // Float64 returns, as a float64, a pseudo-random number in [0.0,1.0).
-func Float64() float64 { return seeder.Float64() }
+func Float64() float64 { return gblRand.Float64() }
 
 // Int returns a non-negative pseudo-random int.
-func Int() int { return seeder.Int() }
+func Int() int { return gblRand.Int() }
 
 // Intn returns, as an int, a non-negative pseudo-random number in [0,n).
 // It panics if n <= 0.
-func Intn(n int) int { return seeder.Intn(n) }
+func Intn(n int) int { return gblRand.Intn(n) }
 
 // Int31 returns a non-negative pseudo-random 31-bit integer as an int32.
-func Int31() int32 { return seeder.Int31() }
+func Int31() int32 { return gblRand.Int31() }
 
 // Int31n returns, as an int32, a non-negative pseudo-random number in [0,n).
 // It panics if n <= 0.
-func Int31n(n int32) int32 { return seeder.Int31n(n) }
+func Int31n(n int32) int32 { return gblRand.Int31n(n) }
 
 // Int63 returns a non-negative pseudo-random 63-bit integer as an int64.
-func Int63() int64 { return seeder.Int63() }
+func Int63() int64 { return gblRand.Int63() }
 
 // Int63n returns, as an int64, a non-negative pseudo-random number in [0,n).
 // It panics if n <= 0.
-func Int63n(n int64) int64 { return seeder.Int63n(n) }
+func Int63n(n int64) int64 { return gblRand.Int63n(n) }
 
 // NormFloat64 returns a normally distributed float64 in the range
 // [-math.MaxFloat64, +math.MaxFloat64] with standard normal distribution
@@ -66,11 +69,11 @@ func Int63n(n int64) int64 { return seeder.Int63n(n) }
 // callers can adjust the output using:
 //
 //		sample = NormFloat64() * desiredStdDev + desiredMean
-func NormFloat64() float64 { return seeder.NormFloat64() }
+func NormFloat64() float64 { return gblRand.NormFloat64() }
 
 // Perm returns, as a slice of n ints, a pseudo-random
 // permutation of the integers [0,n).
-func Perm(n []int) []int { return seeder.Perm(n) }
+func Perm(n int) []int { return gblRand.Perm(n) }
 
 // Uint32 returns a pseudo-random 32-bit value as a uint32.
-func Uint32() uint32 { return seeder.Uint32() }
+func Uint32() uint32 { return gblRand.Uint32() }
